4e2dc0a441c17c46d3b203809cb34f4e083eff5b
Margarita <ritulya.86@mail.ru>
2021-05-02 07:52:36 +0300

    second commit

--- task1.py
+++ task1.py
@@ -1,10 +1,13 @@
 duration = int(input("Введите колличество секунд:  "))
-dey = duration // 86400
+year = duration // 31536000
+dey = (duration % 31536000) // 86400
 hours = (duration % 86400) // 3600
 minutes = (duration % 3600) // 60
 sec = duration % 60
 
-if dey > 0:
+if year > 0:
+    print(f"{year} г {dey} дн {hours} час {minutes} мин {sec} сек")
+elif dey > 0:
     print(f"{dey} дн {hours} час {minutes} мин {sec} сек")
 elif hours > 0:
     print(f"{hours} час {minutes} мин {sec} сек")
--- task3.py
+++ task3.py
@@ -6,3 +6,13 @@ elif 1 < (my_percent % 10) < 5:
 else:
     word = 'процентов'
 print(f'{my_percent} {word}')
+
+my_percent_list = [15, 1, 3, 7, 4, 20]
+for my_percent in my_percent_list:
+    if my_percent % 10 == 1:
+        word = 'процент'
+    elif 1 < (my_percent % 10) < 5:
+        word = 'процента'
+    else:
+        word = 'процентов'
+    print(f'{my_percent} {word}')
